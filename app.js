3856332c9cb68381f700fd7cf810732872eb8732
Luke Goldsworthy <goldswol@oregonstate.edu>
2019-11-23 15:10:21 -0800

    Basic architecture established, essentially a webpage skeleton currently

---
+++ app.js
@@ -0,0 +1,8 @@
+var express = require('express');
+
+var app = express();
+var handlebars = require('express-handlebars').create({defaultLayout:'main'});
+
+app.engine('handlebars', handlebars.engine);
+app.set('view engine', 'handlebars');
+app.set('port', 42066);
