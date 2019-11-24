50b00fc2de01a1771fbbd730fa4c98ef3a6f5a4c
Luke Goldsworthy <goldswol@oregonstate.edu>
2019-11-23 17:00:32 -0800

    added listener and error landing pages

--- app.js
+++ app.js
@@ -11,3 +11,19 @@ app.get('/',function(req,res)
 {
   res.render('home.handlebars');
 });
+
+app.use(function(req,res){
+  res.status(404);
+  res.render('404.handlebars');
+});
+
+app.use(function(err, req, res, next){
+  console.error(err.stack);
+  res.type('plain/text');
+  res.status(500);
+  res.render('500.handlebars');
+});
+
+app.listen(app.get('port'), function(){
+  console.log('Express started on http://localhost:' + app.get('port') + '; press Ctrl-C to terminate.');
+});
