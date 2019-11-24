6ea2dcaf4a02ba4584e76196c4def26f1f204bf1
Luke Goldsworthy <goldswol@oregonstate.edu>
2019-11-23 16:56:27 -0800

    Establishing a home page

--- app.js
+++ app.js
@@ -6,3 +6,8 @@ var handlebars = require('express-handlebars').create({defaultLayout:'main'});
 app.engine('handlebars', handlebars.engine);
 app.set('view engine', 'handlebars');
 app.set('port', 42066);
+
+app.get('/',function(req,res)
+{
+  res.render('home.handlebars');
+});
